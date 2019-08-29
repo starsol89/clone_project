c4e33a39349d1e14c0c0a1d4e350c351c3ac17e0
starsol89 <sinabro1143@gmail.com>
2019-08-30 07:23:16 +0900

    MVC Pattern part Two-2

--- wetube/routers/videoRouter.js
+++ wetube/routers/videoRouter.js
@@ -1,6 +1,13 @@
 import express from "express";
+import routers from "../routes";
 
 const videoRouter = express.Router();
+
+videoRouter.get(routers.videos, (req, res) => res.send("Video"));
+videoRouter.get(routers.upload, (req, res) => res.send("Upload"));
+videoRouter.get(routers.videoDetail, (req, res) => res.send("Video Detail"));
+videoRouter.get(routers.editVideo, (req, res) => res.send("Edit Video"));
+videoRouter.get(routers.deleteVideo, (req, res) => res.send("Delete Video"));
 
 // videoRouter.js 전체를 export 해준다.
 export default videoRouter;
