4e18c4b12717df4971ec527ce570a31f2a58e951
starsol89 <sinabro1143@gmail.com>
2019-09-07 06:54:11 +0900

    Template Variables in Pug

--- wetube/controllers/userController.js
+++ wetube/controllers/userController.js
@@ -13,10 +13,10 @@
     즉 밑에 arrow function에서 무언가 리턴하고 싶다면, 밑에 코드 처럼 작성하면 된다. 정리하면 true를 리턴하는 함수이다.
 */
 
-export const join = (req, res) => res.render("Join");
-export const login = (req, res) => res.render("Login");
-export const logout = (req, res) => res.render("Logout");
-export const users = (req, res) => res.render("Users");
-export const userDetail = (req, res) => res.render("User Detail");
-export const editProfile = (req, res) => res.render("Edit Profile");
-export const changePassword = (req, res) => res.render("Change Password");
+export const join = (req, res) => res.render("join", { pageTitle: "Join"});
+export const login = (req, res) => res.render("login", { pageTitle: "Login"});
+export const logout = (req, res) => res.render("logout", { pageTitle: "Logout"});
+export const users = (req, res) => res.render("users", { pageTitle: "Users"});
+export const userDetail = (req, res) => res.render("userDetail", { pageTitle: "User Detail"});
+export const editProfile = (req, res) => res.render("editProfile", { pageTitle: "Edit Profile"});
+export const changePassword = (req, res) => res.render("changePassword", { pageTitle: "Change Password"});
